5c83a32498d462f66ac7b777ae10ff96a1659cbc
Justin Laureano <gravatron87@gmail.com>
2018-03-24 23:18:59 -0400

    added score rank

--- sandox/app.js
+++ sandox/app.js
@@ -37,7 +37,7 @@ function populate() {
     else {
         //show question
         var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
+        element.innerHTML = quiz.getQuestionIndex().text + "?";
 
         // show choices
         var choices = quiz.getQuestionIndex().choices;
@@ -62,12 +62,13 @@ function guess(id, guess) {
 function showProgress() {
     var currentQuestionNumber = quiz.questionIndex + 1;
     var element = document.getElementById("progress");
-    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length + ".";
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + shuffleQuestions.length + ".";
 };
 
 function showScores() {
     var gameOverHtml = "<h1>Result</h1>";
     gameOverHtml += "<h2 id='score'>Your Score: " + quiz.score + "</h2>";
+    gameOverHtml += "<p id='rank'>" + scoreLevel[quiz.score] + "</p>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHtml;
 };
@@ -77,6 +78,21 @@ function shuffle(array) {
 	return array;
 };
 
+var scoreLevel = [
+    "Your're bloodline has run pure for a 1000 years",
+    "You're a stupid science bitch.",
+    "It seems you must have a tenuous grasp on the human language in general.",
+    "Can I offer you an egg in these trying times?",
+    "alright, alright, alright.",
+    "Oh, I bet you read a lotta Gordon Wood, huh? You read your Gordon Wood and you regurgitate it from a textbook and you think you're wicked awesome doin' that, And how 'bout 'dem apples? And all that Gordon Wood business.",
+    "You haven't even begun to peak.",
+    "When you're doing good in the game, you're doing good in life.",
+    "Your blowing the nips off everybody with your big brains.",
+    "You're a 5 star man!",
+    "You are a true Golden God."
+
+];
+
 var questions = [
     new Question("What is Charlie's last name", ["Kelly", "Smith", "Durant", "Thorton"], "Kelly"),
     new Question("Which of the following does Frank always have on him", ["Rumham", "Crack", "His Gun", "Grilled Charlie"], "His Gun"),
@@ -84,10 +100,18 @@ var questions = [
     new Question("What job did Rickety Cricket used to have", ["Lawyer", "Guitar Player", "Janitor", "Priest"], "Priest"),
     new Question("What is the last name of Ryan and Liam", ["McPoyles", "Mathis", "MacDonald", "Birkowitz"], "McPoyles"),
     new Question("What is Mac's favorite type of coat", ["Pea Coat", "Trench Coat", "Duster", "Varsity Letter jacket"], "Duster"),
+    new Question("What product does Charlie seek investors for", ["Grilled Charlie's", "Kitten Mittens", "Dick Towel", "Project Badass"], "Kitten Mittens"),
+    new Question("What is Charlie's role within the group", ["The Muscle", "The Looks", "The Brains", "The Wildcard"], "The Wildcard"),
+    new Question("Who does Mac & Charlie try to hunt", ["Rickety Cricket", "Bill Ponderosa", "Artemis", "The Waitress"], "Rickety Cricket"),
+    new Question("Who is Charlie in love with", ["Fatty McGoo", "The Waitress", "Gail the Snail", "Dee"], "The Waitress"),
+    new Question("Who does Mac bang that he is ashamed of", ["Gail the Snail", "Artemis", "Carmen", "Mrs. Kelly"], "Carmen"),
+    new Question("Who is Dennis & Dee's annoying cousin", ["Maureen Ponderosa", "Jack Kelly", "Lil Kev", "Gail the Snail"], "Gail the Snail"),
+    new Question("What is Frank's idea for a band name", ["Chemical Toilet", "Pecan Sandies", "Dayman", "The Freight Train"], "Pecan Sandies"),
+    new Question("What year did frank go to Vietnam to open his sweatshop", ["1977", "1985", "1993", "1999"], "1993"),
 ];
 
-shuffleQuestions = shuffle(questions);
+shuffleQuestions = shuffle(questions).slice(0,10);
 
-var quiz = new Quiz(shuffleQuestions.slice(3));
+var quiz = new Quiz(shuffleQuestions);
 
 populate();
